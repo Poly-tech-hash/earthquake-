e90b4187ca44ccfa4c817357f34df34ec57f1e07
Poly-tech-hash <85931519+Poly-tech-hash@users.noreply.github.com>
2021-07-28 09:12:05 +0200

    Add files via upload

---
+++ namelist.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from "React";
+
+export default class users extends Components{
+    state= {
+
+        persons:[['sadi','ledwaba','polokwane',23],
+        ['khomotso','tsebe','masemola',29],
+        ['pearl','khuto','lecowza',16]        ]
+    };
+}
+
+layout = {
+    fontsize: 14,
+    backgroundcolor: 'pink'
+}
+
+render () 
+    return(
+        <div>
+            <h1>morning people</h1>
+            <ul>
+                {this.state.list.map(Element => <li>{Element}</li>)
+                }
+            </ul>
+        </div>
+
+    );
+
+
+getElements()
+    const { names} = this.state;
+    const age = this.state.location
+    return location
+    
+    getAge()
+        //         const l =this.state.Age;
+        //         const messag ='';
+        //        if(l>=29)
+        //        {
+        //             messag=<h1> you are older enough to code</h1>
+        //        }else {
+        //            messag=<h1> you are not older enough to code</h1>
+        //        }
+        // return messag;
+        //     }
+        
+         
+        export default list;
+
+                            
+
